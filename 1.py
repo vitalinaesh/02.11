bf5721af0c06353b37776649033546400128aa69
user <zcel6181@gmail.com>
2024-11-16 19:38:06 +0200

    dz :)

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='logs.log',
+    filemode='w',
+    format='%(levelname)s %(asctime)s - %(message)s'
+)
 class Dimensions:
     def __init__(self,width,height,length):
         self.width = width
@@ -6,9 +14,20 @@ class Dimensions:
 
 class Animal:
     def __init__(self, weight, width, heigth, length, age):
-        self.weight = weight
-        self.dimensions = Dimensions(width, heigth, length)
-        self.age = age
+        try:
+            if weight <= 0:
+                raise ValueError('Weight must be greater than 0')
+            if age <= 0:
+                raise ValueError('ge must be greater than 0')
+
+            self.weight = weight
+            self.dimensions = Dimensions(width, heigth, length)
+            self.age = age
+
+            logging.debug(f'Animal created: Weight={self.weight}, Dimensions=({width}, {height}, {length}), Age={self.age}')
+        except Exception as e:
+            logging.error(f"Error while creating Animal: {e}")
+            raise
 
     def printanimal(self):
         print(f'Animal weight :{self.weight}')
@@ -17,6 +36,15 @@ class Animal:
         print(f'Animal length : {self.dimensions.length}')
         print(f'Animal age : {self.age}')
 
+        logging.debug(f"Animal info: weight={self.weight}, width={self.dimensions.width}, "
+                      f"height={self.dimensions.height}, length={self.dimensions.length}, age={self.age}")
 
-Panda = Animal("70 кг", 1420, 70, "1.2 м", 10)
-Panda.printanimal()
+
+try:
+    Panda = Animal("70 кг", 1420, 70, "1.2 м", 10)
+    randomtvarina = Animal(-10, 70, 55, 88, 89)
+    Panda.printanimal()
+    randomtvarina.printanimal()
+
+except Exception as e:
+    print(f"Error: {e}")
